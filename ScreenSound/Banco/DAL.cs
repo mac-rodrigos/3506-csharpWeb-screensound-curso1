fda8c910c4554625434197552e087b0812fce5e0
mac-rodrigos <rodrigo.soares@mac.com.br>
2024-03-22 14:55:40 -0300

    Modulo 4 Migrations

--- ScreenSound/Banco/DAL.cs
+++ ScreenSound/Banco/DAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScreenSound.Modelos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,43 @@ using System.Threading.Tasks;
 
 namespace ScreenSound.Banco
 {
-    internal class DAL
+    internal class DAL<T> where T : class    //O T representa o tipo genérico
     {
+        protected readonly ScreenSoundContext context;
+
+        public DAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+
+        public  IEnumerable<T> Listar()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Adicionar(T objeto)
+        {
+            context.Set<T>().Add(objeto);
+            context.SaveChanges();
+        }
+
+        public void Atualizar(T objeto)
+        {
+            context.Set<T>().Update(objeto);
+            context.SaveChanges();
+        }
+
+        public void Deletar(T objeto)
+        {
+            context.Set<T>().Remove(objeto);
+            context.SaveChanges();
+        }
+
+        public T? RecuperarPor(Func<T, bool> condicao) 
+        { 
+            return context.Set<T>().FirstOrDefault(condicao);
+        }
+
+
     }
 }
