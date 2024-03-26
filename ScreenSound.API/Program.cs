1f88a0e3a600a256b6b5ea32b688d639e5879c30
mac-rodrigos <rodrigo.soares@mac.com.br>
2024-03-26 14:01:54 -0300

    ALURA API - Arquitetura e API Mínima

---
+++ ScreenSound.API/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddDbContext<ScreenSoundContext>();
+    builder.Services.AddTransient<DAL<Artista>>();
+
+    builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+    var app = builder.Build();
+
+    app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
+    {
+        return Results.Ok(dal.Listar());
+    });
+
+
+    app.MapGet("/Artistas/{nome}", ([FromServices] DAL<Artista> dal, string nome) =>
+    {
+        var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+        if (artista is null)
+        {
+            return Results.NotFound();
+        }
+        return Results.Ok(artista);
+    });
+
+    app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody]Artista artista) =>
+    {
+        dal.Adicionar(artista);
+        return Results.Ok(artista);
+    });
+
+app.Run();
